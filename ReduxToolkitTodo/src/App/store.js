24c7b300597a6e490d70e4793c7d1e06f50eb433
SumanSingha05 <sumansingha8654@gmail.com>
2024-07-23 01:17:08 +0530

    Pre-Final Steps Done

--- ReduxToolkitTodo/src/App/store.js
+++ ReduxToolkitTodo/src/App/store.js
@@ -1,4 +1,7 @@
 //First of all To make store we have to configure store
 import {configureStore} from '@reduxjs/toolkit' //This is core redux not React Redux
+import todoReducer from '../features/todo/todoSlice'
 
-export const store = configureStore({})
+export const store = configureStore({
+    reducer: todoReducer
+})
